8230d31aa6dff61d7c7cefec58cc58a7e0ce83c7
sean <sean@deepracticex.com>
2025-04-08 01:30:13 +0800

    fix: 更新transformer接口导出，添加outputProcessor接口

--- packages/core/src/transformer/interfaces/index.ts
+++ packages/core/src/transformer/interfaces/index.ts
@@ -2,20 +2,15 @@
  * Transformer 接口模块导出
  */
 
-// 测试使用@core别名导入模块
-import { Document } from '@core/types/node';
-
-// 导出所有接口
-export * from './adapterChain';
+export * from './transformer';
+export * from './transformerVisitor';
+export * from './transformContext';
+export * from './transformOptions';
 export * from './adapterSelector';
 export * from './outputAdapter';
 export * from './outputAdapterFactory';
-export * from './outputProcessor';
+export * from './adapterChain';
 export * from './tagProcessor';
 export * from './tagProcessorRegistry';
-export * from './transformContext';
-export * from './transformerOptions';
-export * from './transformOptions';
-export * from './transformer';
 export * from './transformerFactory';
-export * from './transformerVisitor'; 
+export * from './outputProcessor'; 
